2ce1bded92ab8a45db1234130ad6fd515635e5a3
LoneGhost1514 <138025995+LoneGhost1514@users.noreply.github.com>
2024-09-01 10:00:15 +0530

    Update install.sh
    Added structure for different purpose installations such as minimal,full and coding

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -5,21 +5,43 @@ echo "The Installer Is Starting ..."
 echo "Performing System Update"
 $(sudo pacman -Syu)
 
-echo "Installing General Programs"
-$(sudo pacman -S gnome-tweaks brave-browser min firefox cava cmatrix )
+read -p "Select from the following conditions
+1.Full Install
+2.Minimal Install
+3.Coding Workspace
+Enter Your Choice : "  number
 
-echo "Installing Coding Software"
-$(sudo pacman -S code git npm nano vim make python qemu-full virt-manager )
+# Check the input and print the corresponding message
+if [ "$number" -eq 1 ]; then
+    echo "Installing General Programs"
+    $(sudo pacman -S gnome-tweaks brave-browser min firefox cava cmatrix )
 
-echo "Installing Yay"
-$(sudo pacman -S --needed base-devel git)
-$(cd ~)
-$(git clone https://aur.archlinux.org/yay.git)
-$(cd yay)
-$(makepkg -si)
+    echo "Installing Coding Software"
+    $(sudo pacman -S code git npm nano vim make python qemu-full virt-manager )
 
-echo "Installing Additional Software"
-$(sudo pacman -S anydesk zed zen)
+    echo "Installing Yay"
+    $(sudo pacman -S --needed base-devel git)
+    $(cd ~)
+    $(git clone https://aur.archlinux.org/yay.git)
+    $(cd yay)
+    $(makepkg -si)
+
+    echo "Installing Additional Software"
+    $(sudo pacman -S anydesk zed zen)
+
+elif [ "$number" -eq 2 ]; then
+    echo "Setting Up minimal installation"
+    $(sudo pacman -S brave-broser firefox nano code)
+elif [ "$number" -eq 3 ]; then
+   echo "Setting Up Coding Workspace"
+   $(sudo pacman -S code git nano vim make qemu-full virt-manager npm firefox)
+else
+    echo "You have entered a wrong choice"
+    echo "Restart The Script to continue"
+    exit 1
+fi
 
 echo "Performing System Update"
 $(sudo pacman -Syu)
+
+echo "The Programs have been successfully installed"
